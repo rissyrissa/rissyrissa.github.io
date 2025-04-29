1520c5c2b4527a1a4797f649040fb142a96e6d28
Marissa Rochez <rochezmc@gmail.com>
2025-04-29 13:49:26 -0400

    main

--- ROCH_final.js
+++ ROCH_final.js
@@ -24,7 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-
 // Image Modal Functionality
 function setupImageModal() {
     const modal = document.getElementById('imageModal');
